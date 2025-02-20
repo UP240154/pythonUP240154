25f868467c8b57f0588c7908512b3f5dbb30ee60
abner <up240154@alumnos.upa.edu.mx>
2025-02-20 11:40:00 -0600

    anaya

--- Progam3.py
+++ Progam3.py
@@ -1,1 +1,5 @@
-print"hola"
+age = 18
+print ("Tu edad es ", age)
+height = 1.78
+print ("Tu altur es de ", height, " metros")
+
---
+++ Program2.py
@@ -0,0 +1,53 @@
+#Day 2: 30 Days of python programming
+firstname = "Abner"
+lastname = "Ruvalcaba"
+nombre = (firstname + lastname)
+print (nombre)
+country = "México"
+print (country)
+ciudad = "Guadalajara"
+print (ciudad)
+age = 18
+print (age)
+year =  2006
+print (year)
+is_married = False
+is_true = "The sky is blue"
+is_light_on = True
+info = {
+    'fistrname':'Abner',
+    'lastname':'Ruvalcaba',
+    'country':'Mexico'
+}
+
+print(type(firstname))
+print(type(lastname))
+print(type(nombre))
+print(type(country))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(firstname))
+print(len(firstname), len(lastname))
+num_one=5
+num_two=4
+
+totalsum=(num_one + num_two)
+print(totalsum)
+
+totalres=(num_one + num_two)
+print(totalres)
+
+totalmult=(num_one*num_two)
+print(totalmult)
+
+totaldiv=(num_one/num_two)
+print(totaldiv)
+
+
+
+
+
