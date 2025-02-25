abb8d7d4e4c8fae14e9ae6a00f455833265b68e6
abner <up240154@alumnos.upa.edu.mx>
2025-02-25 08:35:19 -0600

    .

--- Program2.py
+++ Program2.py
@@ -47,7 +47,24 @@ print(totalmult)
 totaldiv=(num_one/num_two)
 print(totaldiv)
 
+moddiv=(num_two%num_one)
+print(moddiv)
 
+total_exp = (num_one**num_two)
+print(total_exp)
 
+floor_div=(num_one//num_two)
+print(floor_div)
 
+area=(3.1416*(30**2))
+print("area: ", area)
+
+circum=(60*3.1416)
+print("circunferencia: ", circum)
+
+print("radio?")
+radio=int(input())
+area1=(radio**2)
+area2=area1*3.1416
+print(area2)
 
