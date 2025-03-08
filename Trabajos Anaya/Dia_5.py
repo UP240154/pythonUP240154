4c587f1a5a1fd2b8b6ad9b89e7923e3e21559437
abner <up240154@alumnos.upa.edu.mx>
2025-03-08 15:35:51 -0600

    .

---
+++ Trabajos Anaya/Dia_5.py
@@ -0,0 +1,50 @@
+##
+lst=list()
+
+##
+mandado=['manzana', 'pera', 'platano', 'uva', 'naranja']
+
+##
+print(len(mandado))
+
+##
+print(mandado[0])
+print(mandado[2])
+print(mandado[4])
+
+##
+mixed_data_types=['Abner', '18', '1.75', 'soltero', 'Orion, 116']
+
+##
+it_companies=['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+
+##
+print(it_companies) 
+
+##
+print(len(it_companies))
+
+##
+print(it_companies[0])
+print(it_companies[3])
+print(it_companies[6])
+
+##
+del it_companies[0]
+it_companies.insert(0, 'Twitter')
+print(it_companies)
+
+##
+it_companies.append('IBM')
+
+##
+it_companies.insert(3, 'Samsung')
+
+##
+it_companies[0]=it_companies[0].upper()
+print(it_companies)
+
+##
+print
+
+##
