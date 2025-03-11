25997560da98ce0c42902a483488e1fc129a5fd6
abner <up240154@alumnos.upa.edu.mx>
2025-03-10 21:07:23 -0600

    .

--- Trabajos Anaya/Dia_5.py
+++ Trabajos Anaya/Dia_5.py
@@ -45,6 +45,81 @@ it_companies[0]=it_companies[0].upper()
 print(it_companies)
 
 ##
-print
+print(str.join('#;', it_companies))
 
 ##
+existe='Facebook' in it_companies
+print(existe)
+
+##
+it_companies.sort()
+print(it_companies)
+
+##
+it_companies.reverse()
+print(it_companies)
+
+##
+companies1=it_companies[0:3]
+print(companies1)
+
+##
+companies2=it_companies[6:9]
+print(companies2)
+
+##
+companies3=it_companies[4:5]
+print(companies3)
+
+##
+print(it_companies.remove('Samsung'))
+
+##
+print(it_companies.remove('IBM'))
+
+##
+print(it_companies.remove('Oracle'))
+
+##
+print(it_companies.remove('Amazon'))
+
+##
+ront_end=['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end=['Node', 'Express', 'MongoDB']
+total=ront_end+back_end
+print(total)   
+
+##
+full_stack=total.copy()
+print(full_stack)
+
+##Nivel 2
+
+##
+ages=[19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages)
+del ages[0:2]
+del ages[7:8]
+print(ages)
+ages.append(19)
+ages.append(19)
+ages.append(26)
+print(ages)
+
+prom=(sum(ages)/len(ages))
+print(prom)
+ages.sort()
+print(abs(ages[0]-prom))
+print(abs(ages[8]-prom))
+
+from lista_paises import countries
+print(countries[int(len(countries)/2)])
+list1=countries[0:96]
+list2=countries[96:192]
+
+paises=['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+first_three=paises[0:3] 
+rest=paises[3:7]
+print(first_three)
+print(rest)
