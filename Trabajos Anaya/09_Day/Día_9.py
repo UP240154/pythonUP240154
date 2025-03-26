41c3d9d51a9a444e50074fa05f31b700fc0c7d52
abner <up240154@alumnos.upa.edu.mx>
2025-03-26 09:28:10 -0600

    .

---
+++ Trabajos Anaya/09_Day/Día_9.py
@@ -0,0 +1,37 @@
+##Nivel 1
+
+## 
+age=int(input("Enter your age"))
+if age >= 18:
+        print("You are old enough to drive")
+else:
+        print(18-age)
+
+##
+my_age=19
+your_age=int(input("Enter your age"))
+if my_age>your_age:
+    dif1=my_age-your_age
+    if dif1==1:
+        print("You are 1 year younger than me")
+    else:
+        print("You are ",dif1," years younger than me")
+else:
+    dif2=your_age-my_age
+    if dif2==1:
+        print("You are 1 year older than me")
+    else:
+        print("You are ",dif2," years older than me")
+##
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+if a>b and a==b:
+    if a==b:
+         print("both are equal")
+    else:
+         print(a, "is greater than", b)
+else:
+    print(b, "is greater than", a)
+
+##Level 2
+
