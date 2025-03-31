9e072b1da933589d88f3525ccf41634b4bf2e689
abner <up240154@alumnos.upa.edu.mx>
2025-03-30 19:28:43 -0600

    .

---
+++ Trabajos Anaya/10_Day/Día_10.py
@@ -0,0 +1,43 @@
+##
+count=0
+while count<11:
+    print(count)
+    count=count + 1
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+for i in numbers:
+    print(i)
+
+##
+while -1<count:
+    print(count)
+    count=count - 1
+
+inum=[10,9,8,7,6,5,4,3,2,1]
+for i in inum:
+    print(i)
+
+##
+gato=0
+while gato<8:
+    print('#'*int(gato))
+    gato=gato + 1
+
+##
+fila=0
+columna=8
+
+for i in range(fila):
+    for j in range(columna):
+        print('#', end='')
+    print()
+
+##
+num=0
+
+while num<11:
+    print(num, 'x', num, '=', num*num)
+    num=num + 1
+
+##
+   
