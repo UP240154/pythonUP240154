786c628061858f738b2807afebc481aecc8e56bb
abner <up240154@alumnos.upa.edu.mx>
2025-04-03 21:33:20 -0600

    .

---
+++ Trabajos Anaya/15_Day/Dia_15.py
@@ -0,0 +1,37 @@
+##
+print('hello world')
+
+##
+age = 25
+print(age)
+
+##
+numbers = [1, 2, 3, 4, 5]
+print(numbers[4])
+
+
+##
+import math
+
+##
+print(math.pi)
+
+##
+users = {'name':'Asab', 'age':250, 'country':'Finland'}
+print(users['name'])
+print(users['country'])
+
+
+##
+print(4 + float('3'))
+
+##
+from math import pow
+print(pow(2,3))
+
+##
+print("           ")
+print(int('12'))
+
+
+#print(1/0)
