7f780560a672d98b02d368ec5eed771e19e1d122
abner <up240154@alumnos.upa.edu.mx>
2025-04-03 21:20:23 -0600

    .

--- Trabajos Anaya/14_Day/Dia_14.py
+++ Trabajos Anaya/14_Day/Dia_14.py
@@ -132,5 +132,96 @@ result = reduce(
 print(result)
 
 ##
+def get_string_lists(lst):
+    return list(filter(lambda x: isinstance(x, str), lst))
 
+mixed_list = [1, 'hello', 3.14, 'world', True, 'Python']
+string_list = get_string_lists(mixed_list)
+print(string_list)
 
+##
+from functools import reduce
+
+def add(x, y):
+    return x + y
+
+total = reduce(add, numbers)
+print(total)
+
+##
+from functools import reduce
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+sentence = reduce(
+    lambda acc, country: acc + ', ' + country if country != countries[-1] else acc + ', and ' + country, countries[:-1],countries[0]) + ' are north European countries.'
+print(sentence)
+
+##
+from lista_paises import countries  
+countries_with_land = list(filter(lambda country: 'land' in country.lower(), countries))
+print(countries_with_land)
+
+##
+from lista_paises import countries
+def count_countries_by_letter(countries):
+    country_count = {}
+    for country in countries:
+        first_letter = country[0].upper()  
+        if first_letter in country_count:
+            country_count[first_letter] += 1
+        else:
+            country_count[first_letter] = 1
+    return country_count
+
+country_letter_count = count_countries_by_letter(countries)
+print(country_letter_count)
+
+##
+from lista_paises import countries 
+def get_first_ten_countries(countries):
+    return countries[:10] 
+
+first_ten_countries = get_first_ten_countries(countries)
+print(first_ten_countries)
+
+##
+from lista_paises import countries 
+def get_last_ten_countries(countries):
+    return countries[-10:]  
+
+last_ten_countries = get_last_ten_countries(countries)
+print(last_ten_countries)
+
+##Level 3
+from countries_data import countries_1  # Importing the countries data
+
+#
+def sort_countries_by_name(countries):
+    return sorted(countries, key=lambda x: x['name'])
+
+sorted_countries = sort_countries_by_name(countries_1)
+print("Countries sorted by name:")
+for country in sorted_countries:
+    print(country['name'])
+
+#
+def most_spoken_languages(countries, top_n=10):
+    from collections import Counter
+    language_counter = Counter()
+    for country in countries:
+        language_counter.update(country['languages'])
+    return language_counter.most_common(top_n)
+
+most_spoken = most_spoken_languages(countries_1, 10)
+print("\nTen most spoken languages:")
+for language, count in most_spoken:
+    print(f"{language}: {count}")
+
+#
+def most_populated_countries(countries, top_n=10):
+    return sorted(countries, key=lambda x: x['population'], reverse=True)[:top_n]
+
+most_populated = most_populated_countries(countries_1, 10)
+print("\nTen most populated countries:")
+for country in most_populated:
+    print(f"{country['name']}: {country['population']}")
